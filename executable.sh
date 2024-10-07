8f9829fbc77092a6e4a10f4fb1c5ba6ca92f6754
dani <1745420@uab.cat>
2024-10-07 10:06:57 +0200

    versio8

--- executable.sh
+++ executable.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# Pas 1
 cut -d',' -f1-11,13-15 supervivents.csv > temp.csv
+
+# Pas 2
 awk -F, '$14!="True"' temp.csv > temp2.csv
 a=$(expr $(wc -l < temp.csv) - $(wc -l < temp2.csv))
 echo "S'han eliminat $a registres"
 
+# Pas 3
+awk -F"," 'NR==1 {
+	print $0 ",Ranking_views";
+	next
+}
+{
+	if ($8>=10000000) Ranking_views="Estrella";
+	else if ($8>=1000000) Ranking_views="Excel·lent";
+	else Ranking_views="Bo";
+	print $0 "," Ranking_views
+}' temp2.csv > temp3.csv
+
+# Pas 4
