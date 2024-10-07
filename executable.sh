8a8faa620438b87e2a69c722b182956b50f0753e
dani <1745420@uab.cat>
2024-10-07 08:46:21 +0200

    versio7

--- executable.sh
+++ executable.sh
@@ -2,4 +2,6 @@
 
 cut -d',' -f1-11,13-15 supervivents.csv > temp.csv
 awk -F, '$14!="True"' temp.csv > temp2.csv
-expr $(wc -l < temp.csv) - $(wc -l < temp2.csv)
+a=$(expr $(wc -l < temp.csv) - $(wc -l < temp2.csv))
+echo "S'han eliminat $a registres"
+
